d748cb30401499ca9bc35fc1d565012f918678e6
Ajay Patel <ajpatel2005@gmail.com>
2024-12-02 20:15:42 -0800

    Completed API Fetch to get all stock information for full Portfolio

--- frontend/app/fullPortfolio/page.tsx
+++ frontend/app/fullPortfolio/page.tsx
@@ -26,12 +26,51 @@ const PortfolioPage = () => {
     router.push('/addStock');
   }
 
+  useEffect(() => {
+    const fetchStockData = async () => {
+      fetch('http://127.0.0.1:5001/api/stocks', {
+        method: 'GET',
+        headers: {
+            'Content-type': 'application/json; charset=UTF-8',
+            'Authorization': `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImlhdCI6MTczMzE5NjYwNiwiZXhwIjoxNzMzMjgzMDA2fQ.XUSqcvD-DHU7_R_Npo1p-P5CMSRwnsyajPxJ0abZ2Xw`,
+        },
+      })
+      .then((res) => {   
+        if (!res.ok) {
+            if (res.status === 401)
+            {
+                throw new Error('Invalid credentials: ' + res.statusText);
+            }
+            throw new Error('Login failed: ' + res.statusText);
+        }
+        return res.json();
+      })
+      .then((data) => {
+        console.log(data);
+        setStock(data);
+      })
+      .catch((err) => {
+        console.log(err.message);
+        alert('Login failed. Please try again.');
+      });
+};
+  fetchStockData();
+
+}, []);
+
+
   // Fetch stock data from the API
   
 return (
 
   <div>
-   {}
+    {stock.map((stock, index) => (
+      <li key={index}>
+        {stock.symbol}
+      </li>
+
+    ))
+}
 
 
 <button onClick={(e) => handleAddStock(e)}>
