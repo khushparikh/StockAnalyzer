86d0c68cf62f8ed5846b9ca8f6bc1a2d1f69cd05
alexhyoon <yoon.h.alex@gmail.com>
2024-12-06 14:53:59 -0800

    fixed bug with analyzeStock page, dealing with when a stock cannot be found

--- frontend/app/analyzeStock/page.tsx
+++ frontend/app/analyzeStock/page.tsx
@@ -117,12 +117,12 @@ const AnalyzeStock = () => {
           <div className="bg-gray-800 shadow-lg rounded-lg p-6 overflow-visible">
             <h2 className="text-2xl font-medium mb-4">AI Investment Insights</h2>
             <p className="italic text-gray-300 whitespace-normal leading-relaxed overflow-visible">
-              {stockData.aiAnalysis.split('\n').map((line: string, index: number) => (
+              {stockData.aiAnalysis ? stockData.aiAnalysis.split('\n').map((line: string, index: number) => (
                     <span key={index}>
                     {line}
                     <br />
                     </span>
-                )) || "No AI analysis available at this time."}
+                )) : "No AI analysis available at this time."}
             </p>
           </div>
           {/* Action Button */}
